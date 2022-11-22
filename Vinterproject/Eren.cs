62d283cce5886f3c68e53c6ac72c40c6a80e1819
Johanemsgard <emsgard.johan@elev.ga.ntig.se>
2022-11-22 13:01:56 +0100

    huisgrhufse

--- Vinterproject/Eren.cs
+++ Vinterproject/Eren.cs
@@ -10,6 +10,23 @@ public class Eren
 
     public Eren()
     {
-        
+        sprite = Raylib.LoadTexture("Eren.png");
+        rect = new Rectangle(0,0, sprite.width, sprite.height);
+    }
+
+    //Metod Movement
+    public void Update()
+    {
+        if (Raylib.IsKeyDown(KeyboardKey.KEY_RIGHT))
+        {
+            rect.x += speed;
+        }
+
+    }
+
+    //Metod
+    public void Draw()
+    {
+        Raylib.DrawTexture(sprite, (int) rect.x, (int)rect.y, Color.WHITE);
     }
 }
--- Vinterproject/Program.cs
+++ Vinterproject/Program.cs
@@ -4,8 +4,19 @@
 Raylib.InitWindow(1000,800, "Scene");
 Raylib.SetTargetFPS(60);
 
+Eren eren = new Eren();
+
+
 while(!Raylib.WindowShouldClose())
 {
+    //Logik
+    eren.Update();
+
+
+    //Garfik
+    Raylib.BeginDrawing();
+    Raylib.ClearBackground(Color.WHITE);
+    eren.Draw();
 
 
 
