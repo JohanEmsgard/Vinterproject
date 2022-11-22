74994136b3fe31df1ee737d84c25c0dbcd5359ae
Johanemsgard <emsgard.johan@elev.ga.ntig.se>
2022-11-22 12:50:30 +0100

    gvhfdsfeghdfs

---
+++ Vinterproject/Eren.cs
@@ -0,0 +1,15 @@
+using System;
+
+public class Eren
+{
+    //Postion 
+    private Rectangle rect;
+    private Texture2D sprite;
+
+    public float speed = 5f;
+
+    public Eren()
+    {
+        
+    }
+}
--- Vinterproject/Program.cs
+++ Vinterproject/Program.cs
@@ -1,2 +1,13 @@
-﻿Console.WriteLine("Hello, World!");
+﻿global using Raylib_cs;
 
+//Skärm
+Raylib.InitWindow(1000,800, "Scene");
+Raylib.SetTargetFPS(60);
+
+while(!Raylib.WindowShouldClose())
+{
+
+
+
+    Raylib.EndDrawing();
+}
