34253d72b72dca989ecdfe962bd7fd16dc241bca
Johanemsgard <emsgard.johan@elev.ga.ntig.se>
2022-11-30 13:32:35 +0100

    njkfdjkdv

--- Vinterproject/Form.cs
+++ Vinterproject/Form.cs
@@ -1,8 +1,27 @@
 using System;
+using System.Numerics;
 
 public class Form
 {
-    protected Texture2D sprite;
     private Rectangle rect;
-    
+    protected Texture2D sprite;
+
+    public Form()
+    {
+        sprite = Raylib.LoadTexture("Eren.png");
+
+    }
+
+    public void Update(Vector2 pos)
+    {
+        rect.x = pos.X - rect.width/2;
+        rect.y = pos.Y - rect.width/2;
+
+    }
+
+    public void Draw()
+    {
+        Raylib.DrawTexture(sprite, (int)rect.x, (int)rect.y, Color.WHITE);
+    }
+
 }
